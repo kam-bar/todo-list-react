a67c09e0d89f16417dbeec82f30cb2353134d03b
Kamil <kam.barabasz@gmail.com>
2025-11-15 23:06:14 +0100

    add Tasks component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Form from "./components/Form";
+import Tasks from "./components/Tasks";
 
 function App() {
+    const tasks = [
+        { id: 1, content: "przejść na Reacta", done: false },
+        { id: 2, content: "zjeść obiad", done: true },
+        { id: 3, content: "napić się kawy", done: false },
+    ];
+
     return (
         <Container>
             <Header title="Lista zadań" />
-            <Section
-                title="Dodaj nowe zadanie"
-                body={<Form />}
-            />
+            <Section title="Dodaj nowe zadanie" body={<Form />} />
+            <Section title="Lista zadań" body={<Tasks tasks={tasks} />} />
         </Container>
     );
 }

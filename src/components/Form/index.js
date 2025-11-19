102ed9a004923ac00604ca947bdfbdfbe4399c10
Kamil <kam.barabasz@gmail.com>
2025-11-19 22:18:59 +0100

    implement controlled form for adding new tasks

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "./Form.css";
 
-const Form = () => (
-    <form className="form">
-        <input
-            className="form__input"
-            type="text"
-            placeholder="Co jest do zrobienia?"
-            autoFocus
-        />
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
-);
+const Form = ({ addNewTask }) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const trimmedContent = newTaskContent.trim();
+
+        if (!trimmedContent) {
+            return;
+        }
+
+        addNewTask(trimmedContent);
+
+        setNewTaskContent("");
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                value={newTaskContent}
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    );
+};
 
 export default Form;

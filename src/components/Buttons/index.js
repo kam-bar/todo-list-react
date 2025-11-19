4de3b3ddb8651fe19023495ad559ccd32745863a
Kamil <kam.barabasz@gmail.com>
2025-11-19 22:51:29 +0100

    add interaction to control buttons

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,19 +1,24 @@
 import "./Buttons.css";
 
-const Buttons = ({ tasks }) => (
-    <div className="buttons">
-        {tasks.length > 0 && (
-            <>
-                <button className="buttons__button">Ukryj ukończone</button>
-                <button
-                    className="buttons__button"
-                    disabled={tasks.every((task) => task.done)}
-                >
-                    Ukończ wszystkie
-                </button>
-            </>
-        )}
-    </div>
-);
+const Buttons = ({ tasks, setAllDone }) => {
+    if (tasks.length === 0) {
+        return null;
+    }
+
+    const allDone = tasks.every(({ done }) => done);
+
+    return (
+        <div className="buttons">
+            <button className="buttons__button">Ukryj ukończone</button>
+            <button
+                className="buttons__button"
+                onClick={setAllDone}
+                disabled={allDone}
+            >
+                Ukończ wszystkie
+            </button>
+        </div>
+    );
+};
 
 export default Buttons;
